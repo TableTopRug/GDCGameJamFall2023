9341f131eec5d30d29975169cec95794a5e29d22
Alexander Lafontaine <71290520+AlphaAlex117@users.noreply.github.com>
2024-08-17 22:51:49 -0400

    Comments for AlleyMap2

--- Clubber Holmes/Assets/Scripts/AlleyMap/AlleyMap2.cs
+++ Clubber Holmes/Assets/Scripts/AlleyMap/AlleyMap2.cs
@@ -5,10 +5,19 @@ using Unity.Mathematics;
 using UnityEngine.Tilemaps;
 using UnityEngine;
 
-
+/*
+ * Generates the Alley Map.
+ * 
+ */
 public class AlleyMap2 : MonoBehaviour
 {
+    // Width and height of the tilemap.
+    /*
+     * Movement amount says how much does the DFS algorithm jumps between coordinates.
+     * Movement cannot be 1 or the DFS is going to set most of the grid as false.
+     */ 
     public int width, height, movementAmount;
+    // Holds whether or not there should be tiles.
     public bool[,] tiles;
 
     // Player starting position.
@@ -22,20 +31,30 @@ public class AlleyMap2 : MonoBehaviour
     // Tiles
     public RuleTile buildingTile;
 
+
     public float WitnessProbability;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Clear the tiles from the alley.
         clearAlley();
 
+        // Generate a grid with the connected path.
         tiles = GenerateAlley(width, height);
 
+        // Generate random coordinates where the player starts.
         startingX = Rand(width);
         startingY = Rand(height);
 
+        // Get a minimum difference so that the end doesn't spawn next to the start.
         int minDiff = Mathf.Max(width, height) / 2;
 
+        /*
+         * While true, then:
+         * 1) Generate random coordinates for the end.
+         * 2) If the difference between the start and end is greater than the minimum difference, then break.
+         */
         while (true)
         {
             endingX = Rand(width);
@@ -44,7 +63,7 @@ public class AlleyMap2 : MonoBehaviour
             if (Mathf.Abs(endingY - startingY) >= minDiff) { break; }
         }
 
-        // For all space in grid, put a tile.
+        // For all coordinates in grid, put a tile.
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -53,7 +72,7 @@ public class AlleyMap2 : MonoBehaviour
             }
         }
 
-        // For all spaces between grid, remove tile depending on where there is no tile.
+        // For all coordinates in grid, remove tile if the tile's coordinate is false.
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -65,7 +84,8 @@ public class AlleyMap2 : MonoBehaviour
             }
         }
 
-        // For all space in grid, put a tile if it has a tile nearby.
+        // *** I don't remember why I added this. ***
+        // For all false coordinates in grid, put a tile if it has a tile next to it. 
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -100,7 +120,8 @@ public class AlleyMap2 : MonoBehaviour
             }
         }
 
-        // For all spaces between grid, remove tile depending on where there is no tile.
+        // *** I don't remember why I added this. ***
+        // For all spaces between grid, remove tile if it has no tiles next to it. 
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -118,13 +139,22 @@ public class AlleyMap2 : MonoBehaviour
         }
     }
 
-
+    /*
+     * Generates alley "room" with corners prepared
+     * Returns a grid that contains the coordinates with tile and with no tile.
+     */
     public bool[,] GenerateAlley(int w, int h)
     {
+        // Size of the alley including borders.
         bool[,] grid = new bool[w+2, h+2];
-
+        
+        // Keeps track of the the visited coordinates.
         bool[,] visited = new bool[w, h];
 
+        /*
+         * Recursive Depth First Search algorithm to:
+         * 1) Create a path until you hit the corners or has been visited.
+         */
         bool dfs(int x, int y)
         {
             if (x >= w ||  y >= h)
@@ -145,6 +175,7 @@ public class AlleyMap2 : MonoBehaviour
                 (x, y+movementAmount, grid, x, y),
             };
 
+            // *** I think this is a calculation from online. To make it more random or something. ***
             foreach (var (nx, ny, g, wx, wy) in dirs.OrderBy(t => frand()))
             {
                 grid[wx, wy] = !(0 <= nx && nx < w && 0 <= ny && ny < h && (dfs(nx, ny) || frand() < WitnessProbability));
@@ -153,6 +184,7 @@ public class AlleyMap2 : MonoBehaviour
             return true;
         }
 
+        // Fill corner tiles.
         for (int i = 0; i < width; i++ )
         {
             tilemap.SetTile(new Vector3Int(i, -1, 0), buildingTile);
@@ -162,28 +194,36 @@ public class AlleyMap2 : MonoBehaviour
             tilemap.SetTile(new Vector3Int(width, i, 0), buildingTile);
         }
 
-
+        // Start DFS algorithm.
         dfs(0, 0);
 
         return grid;
     } 
 
-
-
-    // Return random number.
+    // Return random integer.
     private int Rand(int max)
     {
         return UnityEngine.Random.Range(0, max);
     }
 
+    // Return random float.
     private float frand()
     {
         return UnityEngine.Random.value;
     }
 
+    // Clear all the tiles related to the tilemap.
     private void clearAlley()
     {
         // Clear all tiles
         tilemap.ClearAllTiles();
     }
 }
+
+/***
+ * ADDITIONAL SUGGESTIONS:
+ * 1) I think that the DFS algorithm needs to be changed.
+ * 2) From lines 88-140, I think I was trying to make sure the maze didn't have holes and missing texture.
+ * 3) I think the clear alley was a way to reset the alley generation.
+ * 4) Code from lines 66-85 could be merged.
+ ***/
